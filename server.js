6349bf53e6a66abfbb19d5993fb28e316da84fa8
MAC <bashorunbethel99@gmail.com>
2021-12-13 11:40:27 -0800

    added server and package

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require( 'express' )
+
+function requireHTTPS ( req, res, next )
+{
+    // The 'x-forwarded-proto' check is for Heroku
+    if ( !req.secure && req.get( 'x-forwarded-proto' ) !== 'https' )
+    {
+        return res.redirect( 'https://' + req.get( 'host' ) + req.url )
+    }
+    next()
+}
+const app = express()
+app.use( requireHTTPS )
+
+app.use( express.static( './dist/mortgage' ) )
+
+app.get( '/*', function ( req, res )
+{
+    res.sendFile( 'index.html', { root: 'dist/mortgage' } )
+} )
+
+app.listen( process.env.PORT || 8080 )
